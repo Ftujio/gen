1fc07377065ba1f92e994ba706935dc8c7f29fcf
Ftujio <svetlin954@abv.bg>
2016-12-22 10:58:26 +0200

    Added "ReactiveFormsModule".

--- app/app.module.js
+++ app/app.module.js
@@ -12,6 +12,7 @@ var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
 var forms_1 = require('@angular/forms');
 var http_1 = require('@angular/http');
+var forms_2 = require('@angular/forms');
 var app_component_1 = require('./app.component');
 var phenotype_component_1 = require('./phenotype.component');
 var phenotype_form_component_1 = require('./phenotype.form.component');
@@ -20,7 +21,7 @@ var AppModule = (function () {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule, forms_1.FormsModule, http_1.HttpModule],
+            imports: [platform_browser_1.BrowserModule, forms_1.FormsModule, forms_2.ReactiveFormsModule, http_1.HttpModule],
             declarations: [app_component_1.AppComponent, phenotype_component_1.PhenotypeComponent, phenotype_form_component_1.PhenotypeFormComponent],
             bootstrap: [app_component_1.AppComponent]
         }), 
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';	
 
 import { AppComponent }  from './app.component';
 import { PhenotypeComponent }  from './phenotype.component';
 import { PhenotypeFormComponent } from './phenotype.form.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpModule ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule , HttpModule ],
   declarations: [ AppComponent, PhenotypeComponent, PhenotypeFormComponent ],
   bootstrap:    [ AppComponent ]
 })
