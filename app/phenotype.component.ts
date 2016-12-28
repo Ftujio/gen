10e81328d7ab2f0af82fbe26d66744acd72a39b2
Ftujio <svetlin954@abv.bg>
2016-12-28 11:39:46 +0200

    Removed "directives" property of the component because it's deprecated.

--- app/phenotype.component.ts
+++ app/phenotype.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 
-import { PhenotypeFormComponent } from './phenotype.form.component';
+//import { PhenotypeFormComponent } from './phenotype.form.component';
 
 @Component({
 	selector: 'phenotype',
-	templateUrl: 'app/templates/phenotype.component.html',
-	directives: [PhenotypeFormComponent]
+	templateUrl: 'app/templates/phenotype.component.html'
 })
-
 export class PhenotypeComponent{
 	
 }
