6c7940b8e6a7388411315e4f38a04b31e787269f
Ftujio <svetlin954@abv.bg>
2016-12-29 13:53:40 +0200

    Added "Injectable" module.

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Injectable } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';	
 
 import { AppComponent }  from './app.component';
@@ -9,7 +10,7 @@ import { PhenotypeComponent }  from './phenotype.component';
 import { PhenotypeFormComponent } from './phenotype.form.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule , HttpModule ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule , HttpModule, Injectable ],
   declarations: [ AppComponent, PhenotypeComponent, PhenotypeFormComponent ],
   bootstrap:    [ AppComponent ]
 })
