da98c1417de3063be9e6591968563a55c2abbf66
Ftujio <svetlin954@abv.bg>
2017-01-30 23:00:04 +0200

    Removed the loading of unused modules.

--- src/app/main-form/main-form.module.ts
+++ src/app/main-form/main-form.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 // Components
 import { FormGroupComponent } from './form-group/form-group.component';
