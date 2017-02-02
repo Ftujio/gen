f66bcda18a16d263b84336fec51cc0856b55cfab
Ftujio <svetlin954@abv.bg>
2017-02-02 20:52:41 +0200

    "VariantTypes" component working.

--- src/app/main-form/variant-types/variant-types.component.ts
+++ src/app/main-form/variant-types/variant-types.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormService } from '../services/form.service';
+import { NgForm } from '@angular/forms';
+
 @Component({
   selector: 'app-variant-types',
   templateUrl: './variant-types.component.html',
@@ -7,9 +10,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VariantTypesComponent implements OnInit {
 
-  constructor() { }
+	data: Object;
+	numOfChecked: number = 0;
+	message: String = 'Check at least one checkbox';
 
-  ngOnInit() {
-  }
+	constructor(private formService: FormService) {
+		this.formService.getVariantTypes().subscribe(
+			data => {
+				this.data = data;
+				//console.log(this.data);
+			}
+		);
+	}
+
+	ngOnInit() {
+		
+	}
+
+	onSubmit(form: NgForm){
+		console.log(form.value);
+	}
+
+	setMessage(){
+		if(this.numOfChecked == 0){
+			this.message = 'Check at least one checkbox';
+		} else if(this.numOfChecked < this.data['length']){
+			this.message = 'Check all the checkboxes';
+		} else {
+			this.message = 'All the checkboxes are checked';
+		}
+	}
+
+	checkStatus(form: NgForm){
+		this.numOfChecked = 0;
+
+		for(let key in form.value){
+			let value = form.value[key];
+			//console.log("\"", key, "\"", ": \"", value, "\"");
+			if(value == true){
+				this.numOfChecked++;
+			}
+		}
+
+		this.setMessage();
+	}
+
+	checkAll(form: NgForm){
+		for(let key in form.value){
+			let value = form.value[key];
+			if(value == false || value == undefined){
+				form.controls[key].setValue(true);
+				this.numOfChecked++;
+			}
+		}
+
+		this.setMessage();
+	}
+
+	checkNone(form: NgForm){
+		for(let key in form.value){
+			let value = form.value[key];
+			if(value == true){
+				form.controls[key].setValue(false);
+				this.numOfChecked--;
+			}
+		}
+
+		this.setMessage();
+	}
 
 }
