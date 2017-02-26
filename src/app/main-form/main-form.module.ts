fd23b7c4bf67168d6b2360bd9cdbe9bb5077ea31
Ftujio <svetlin954@abv.bg>
2017-02-26 14:31:29 +0200

    Refactor.

--- src/app/main-form/main-form.module.ts
+++ src/app/main-form/main-form.module.ts
@@ -8,15 +8,15 @@ import { PhenotypeFormComponent } from './phenotype-form/phenotype-form.componen
 import { EffectTypesFormComponent } from './effect-types-form/effect-types-form.component';
 import { PresentInChildFormComponent } from './present-in-child-form/present-in-child-form.component';
 import { PresentInParentFormComponent } from './present-in-parent-form/present-in-parent-form.component';
-
-// Services
-import { FormService } from './services/form.service';
-import { SearchService } from './services/search.service';
 import { ChildGenderComponent } from './child-gender/child-gender.component';
 import { VariantTypesComponent } from './variant-types/variant-types.component';
 import { StudyTypesComponent } from './study-types/study-types.component';
 import { GeneFormComponent } from './gene-form/gene-form.component';
 import { SearchResultComponent } from './search-result/search-result.component';
+
+// Services
+import { FormService } from './services/form.service';
+import { SearchService } from './services/search.service';
 
 @NgModule({
 	imports: [
