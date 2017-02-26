146e031f996d4d74935c3669532d3df5b3eefaa3
Ftujio <svetlin954@abv.bg>
2017-02-26 13:01:00 +0200

    Passing form data to the search component is successful.

--- src/app/main-form/search-result/search-result.component.ts
+++ src/app/main-form/search-result/search-result.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search-result',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchResultComponent implements OnInit {
 
-  constructor() { }
+	@Input() formValue: any;
 
-  ngOnInit() {
-  }
+	constructor() {}
+
+	ngOnInit() {
+	}
 
 }
