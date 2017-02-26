9d40aebbb202e5383cb38dbf993d2bf2a570d58f
Ftujio <svetlin954@abv.bg>
2017-02-26 14:28:26 +0200

    The service is ready.

--- src/app/main-form/services/search.service.ts
+++ src/app/main-form/services/search.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class SearchService {
 
-  constructor() { }
+	constructor(private http: Http) {}
+
+	getData(){
+		return this.http.get('response.json').map(respone => respone.json());
+	}
 
 }
