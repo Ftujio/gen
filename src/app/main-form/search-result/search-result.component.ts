03f3d202d8457c2a1f9c49432a4f3ab3576bcf6c
Ftujio <svetlin954@abv.bg>
2017-02-26 15:59:25 +0200

    Some progress.

--- src/app/main-form/search-result/search-result.component.ts
+++ src/app/main-form/search-result/search-result.component.ts
@@ -10,15 +10,36 @@ export class SearchResultComponent implements OnInit {
 
 	@Input() formValue: any;
 
-	tableData: Object;
-	allData: JSON;
+	tableData: Object[];
+	allData: Object;
 
 	constructor(private searchService: SearchService) {
 		this.searchService.getData().subscribe(
 			data => {
 				this.allData = data;
+				this.handleData();
 			}
 		)
+
+		this.tableData = [
+			{
+				values: []
+			}
+		]
+
+		console.log(this.tableData);
+	}
+
+	handleData(){
+		let cell = {
+			values: []
+		};
+
+		for(let i = 0; i < this.allData['count']; i++){ // One cycle is one row
+			cell.values[0] = this.allData['rows'][i][0]; // family id
+			cell.values[1] = this.allData['rows'][i][1]; // study
+			this.tableData.push(cell);
+		}
 	}
 
 	ngOnInit() {
