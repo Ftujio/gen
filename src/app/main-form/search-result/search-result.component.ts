f9094843a1afcf1c26fb23c7d580677918c47281
Ftujio <svetlin954@abv.bg>
2017-02-26 16:34:30 +0200

    The tableData array is populated with the right data from the response file.

--- src/app/main-form/search-result/search-result.component.ts
+++ src/app/main-form/search-result/search-result.component.ts
@@ -22,23 +22,63 @@ export class SearchResultComponent implements OnInit {
 		)
 
 		this.tableData = [
-			{
-				values: []
-			}
+			/*{ // each roll
+				cells: [ 
+					{ // each cell
+						values: [] // content in the cell
+					}
+				]
+			}*/
 		]
 
 		console.log(this.tableData);
 	}
 
 	handleData(){
-		let cell = {
-			values: []
-		};
+		let row = {
+			cells: [
+				{
+					values: []
+				},
+				{
+					values: []
+				},
+				{
+					values: []
+				},
+				{
+					values: []
+				},
+				{
+					values: []
+				},
+				{
+					values: []
+				}
+			]
+		}
 
 		for(let i = 0; i < this.allData['count']; i++){ // One cycle is one row
-			cell.values[0] = this.allData['rows'][i][0]; // family id
-			cell.values[1] = this.allData['rows'][i][1]; // study
-			this.tableData.push(cell);
+			row.cells[0].values[0] = this.allData['rows'][i][0]; // family id
+			row.cells[0].values[1] = this.allData['rows'][i][1]; // study
+
+			row.cells[1].values[0] = this.allData['rows'][i][3]; // location
+			row.cells[1].values[1] = this.allData['rows'][i][4]; // variant
+
+			row.cells[2].values[0] = this.allData['rows'][i][8]; // worst effect type
+			row.cells[2].values[1] = this.allData['rows'][i][9]; // genes
+
+			row.cells[3].values[0] = this.allData['rows'][i][18]; // SSCfreq
+			row.cells[3].values[1] = this.allData['rows'][i][19]; // EVSfreq
+			row.cells[3].values[0] = this.allData['rows'][i][20]; // E65freq
+
+			row.cells[4].values[0] = this.allData['rows'][i][22].split(":")[0]; // mom
+			row.cells[4].values[1] = this.allData['rows'][i][22].split(":")[1]; // dad
+
+			row.cells[5].values[0] = this.allData['rows'][i][24]; // proband verbal iq
+			row.cells[5].values[1] = this.allData['rows'][i][25]; // proband non-verbal iq
+
+			this.tableData.push(row);
 		}
 	}
 
