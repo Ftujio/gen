d84bd0f0dbe05585c4798389cf312020440e11e8
Ftujio <svetlin954@abv.bg>
2017-02-26 14:49:29 +0200

    Fetching the data from the service.

--- src/app/main-form/search-result/search-result.component.ts
+++ src/app/main-form/search-result/search-result.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { SearchService } from '../services/search.service';
 
 @Component({
   selector: 'app-search-result',
@@ -9,7 +10,16 @@ export class SearchResultComponent implements OnInit {
 
 	@Input() formValue: any;
 
-	constructor() {}
+	tableData: Object;
+	allData: JSON;
+
+	constructor(private searchService: SearchService) {
+		this.searchService.getData().subscribe(
+			data => {
+				this.allData = data;
+			}
+		)
+	}
 
 	ngOnInit() {
 	}
