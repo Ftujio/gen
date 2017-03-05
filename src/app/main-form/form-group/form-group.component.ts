c636641816280848f11d7d6d92911461a7498ec0
Ftujio <svetlin954@abv.bg>
2017-03-05 19:17:59 +0200

    The validation is almost done.

--- src/app/main-form/form-group/form-group.component.ts
+++ src/app/main-form/form-group/form-group.component.ts
@@ -9,6 +9,9 @@ export class FormGroupComponent implements OnInit {
 
 	value: any;
 	display: boolean = false;
+	numOfValid: number = 0;
+	trueDisplay: boolean = false;
+	checked: Array<boolean> = [false, false, false, false, false];
 
 	constructor() {
 		this.value = {
@@ -20,19 +23,70 @@ export class FormGroupComponent implements OnInit {
 			"effect-types": "",
 			"valid": ""
 		};
+
+
 	}
 
-	handleValidity(valid){
-		console.log(valid);
+	handleValidity(valid, num){
+		if(!this.checked[num-1]){
+			this.checked[num-1] = true;
+
+			if(valid == 'VALID'){
+				this.numOfValid++;
+				this.checked[num-1] = true;
+			} else {
+				this.numOfValid--;
+				this.checked[num-1];
+			}
+		}
+
+		if(this.checkNum(num)){
+			this.numOfValid--;
+		}
+
+		this.checkAllValidity();
+	}
+
+	checkNum(num):boolean {
+		let key: string;
+		let empty: boolean = true;
+
+		if(num == 1) key = 'phenotype';
+		if(num == 2) key = 'child-gender';
+		if(num == 3) key = 'variant-types';
+		if(num == 4) key = 'study-types';
+		if(num == 5) key = 'effect-types';
+
+		//console.log('key: ', key);
+
+		for(let data in this.value[key]){
+			//console.log(data , ': ', this.value[key][data]);
+			if(this.value[key][data] == true) {
+				empty = false;
+			}
+		}
+
+		console.log('empty: ', empty);
+
+		return empty;
+	}
+
+	checkAllValidity(){
+		if(this.numOfValid == 5){
+			this.display = true;
+		}
+
+		console.log(this.numOfValid);
 	}
 
 	ngOnInit() {
 	}
 
 	submitForm(formValue){
-
-
-		this.display = true;
+		this.checkAllValidity();
+		if(this.display){
+			this.value['valid'] = true;
+		}
 		console.log(this.value);
 	}
 
