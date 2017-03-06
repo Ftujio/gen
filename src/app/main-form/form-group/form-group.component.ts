96ad423095939a023355f113d4a95231207d656b
Ftujio <svetlin954@abv.bg>
2017-03-06 16:27:17 +0200

    Changed the validity check algorytm in the "FormGroup" component and now it works flawlessly.

--- src/app/main-form/form-group/form-group.component.ts
+++ src/app/main-form/form-group/form-group.component.ts
@@ -9,9 +9,7 @@ export class FormGroupComponent implements OnInit {
 
 	value: any;
 	display: boolean = false;
-	numOfValid: number = 0;
-	trueDisplay: boolean = false;
-	checked: Array<boolean> = [false, false, false, false, false];
+	validForms: Array<string> = ['INVALID', 'INVALID', 'INVALID', 'INVALID', 'INVALID'];
 
 	constructor() {
 		this.value = {
@@ -23,68 +21,25 @@ export class FormGroupComponent implements OnInit {
 			"effect-types": "",
 			"valid": ""
 		};
-
-
 	}
 
-	handleValidity(valid, num){
-		if(!this.checked[num-1]){
-			this.checked[num-1] = true;
+	handleValidity(value, num){
+		num -= 1;
+		this.validForms[num] = value;
+		console.log(this.validForms);
 
-			if(valid == 'VALID'){
-				if(this.numOfValid < 5) this.numOfValid++;
-				this.checked[num-1] = true;
-			} else {
-				if(this.numOfValid > 0) this.numOfValid--;
-				this.checked[num-1];
+		this.display = true;
+		for(let elem in this.validForms){
+			if(this.validForms[elem] == 'INVALID'){
+				this.display = false;
 			}
 		}
-
-		if(this.checkNum(num)){
-			console.log('valid num: ', this.numOfValid);
-			if(this.numOfValid > 0) this.numOfValid--;
-			this.checked[num-1] = false;
-		}
-
-		this.checkAllValidity();
-	}
-
-	checkNum(num):boolean {
-		let key: string;
-		let empty: boolean = true;
-
-		if(num == 1) key = 'phenotype';
-		if(num == 2) key = 'child-gender';
-		if(num == 3) key = 'variant-types';
-		if(num == 4) key = 'study-types';
-		if(num == 5) key = 'effect-types';
-
-		//console.log('key: ', key);
-		for(let data in this.value[key]){
-			//console.log(data , ': ', this.value[key][data]);
-			if(this.value[key][data] == true) {
-				empty = false;
-			}
-		}
-
-		console.log('empty: ', empty);
-
-		return empty;
-	}
-
-	checkAllValidity(){
-		if(this.numOfValid == 5){
-			this.display = true;
-		}
-
-		console.log('[VALID]: ', this.numOfValid);
 	}
 
 	ngOnInit() {
 	}
 
 	submitForm(formValue){
-		this.checkAllValidity();
 		if(this.display){
 			this.value['valid'] = true;
 		}
