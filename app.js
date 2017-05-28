79b43c0b27b616ec7df9a28241e3bac786794314
Ftujio <svetlin954@abv.bg>
2017-05-28 23:16:59 +0300

    Created a route for handling form requests.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+var forms = require('./routes/forms');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api', forms);
 app.use('/', index);
 
 // catch 404 and forward to error handler
