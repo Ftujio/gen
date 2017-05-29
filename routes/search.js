d41662153c59d6126cd71429510147ccc37784af
Ftujio <svetlin954@abv.bg>
2017-05-29 13:23:06 +0300

    Search request works.
    * The data is stored in the database.
    * The connection with the database works.

--- routes/search.js
+++ routes/search.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb').MongoClient
 
 router.get('/search', function(req, res, next){
-	
+	console.log('search request');
+
+	mongo.connect('mongodb://localhost:27017/gen', function (err, db){
+		if(err) res.json({success: false, err: err});
+
+		db.collection('search').find().toArray(function (err, result) {
+    	if(err) res.json({success: false, err: err});
+
+    	res.json(result);
+  	})
+	});
 });
 
 module.exports = router;
